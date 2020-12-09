fe4a1722b5eb55a38e8cc2d2ba147e90123ba5f8
kevinnam <70129171+kevinnam@users.noreply.github.com>
2020-12-09 12:00:52 +0100

    general clean up, and fixed render note

--- scripts/tinyInitialize.js
+++ scripts/tinyInitialize.js
@@ -1,4 +1,3 @@
-console.log('tjenixen')
 tinymce.init({
     init_instance_callback: function(editor) {
         editor.on('input', function() {
@@ -9,7 +8,25 @@ tinymce.init({
     inline: true,
     toolbar: false,
     menubar: false,
-    plugins: 'quickbars',
+    plugins: 'quickbars lists textpattern',
+    textpattern_patterns: [
+        {start: '*', end: '*', format: 'italic'},
+        {start: '_', end: '_', format: 'italic'},
+        {start: '**', end: '**', format: 'bold'},
+        {start: '__', end: '__', format: 'bold'},
+        {start: '#', format: 'h1'},
+        {start: '##', format: 'h2'},
+        {start: '###', format: 'h3'},
+        {start: '####', format: 'h4'},
+        {start: '#####', format: 'h5'},
+        {start: '######', format: 'h6'},
+        {start: '1. ', cmd: 'InsertOrderedList'},
+        {start: '* ', cmd: 'InsertUnorderedList'},
+        {start: '- ', cmd: 'InsertUnorderedList'}
+    ],
+	quickbars_selection_toolbar: 'bold italic underline strikethrough | numlist bullist | quickimage',
     quickbars_insert_toolbar: false,
     placeholder: 'Enter note here'
 });
+
+console.log('tjenixen')
